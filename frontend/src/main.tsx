6275e79d8a75d34e5e55e1b834268b0d5efe886c
larabashadi <71298900+larabashadi@users.noreply.github.com>
2026-01-24 13:44:27 +0100

    Wrap App component with ErrorBoundary

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
@@ -44,5 +44,17 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
+  </React.StrictMode>
+);
+*/
+import { ErrorBoundary } from "./ErrorBoundary";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
