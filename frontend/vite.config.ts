42aec26e45d89970f008ec493b9c5ae49c875a38
larabashadi <71298900+larabashadi@users.noreply.github.com>
2026-01-24 14:03:35 +0100

    Configure build options and esbuild settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,4 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false,
+    sourcemap: true,
+  },
+  esbuild: {
+    keepNames: true,
+  },
 });
