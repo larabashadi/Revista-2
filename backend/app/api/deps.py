a6f09d395cba8036a6a4e64c7bc34b3a5cc21f36
larabashadi <71298900+larabashadi@users.noreply.github.com>
2026-01-28 11:37:09 +0100

    Update deps.py

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -8,8 +8,7 @@ from sqlalchemy.orm import Session
 
 from app.core.db import SessionLocal
 from app.core.security import decode_token
-from app.models.models import User
-
+from app.models.models import User, Club, Subscription
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 
